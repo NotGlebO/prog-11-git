2a9df4601285461950d12e4b49f94abb21abe6d7
NotGlebO <77108978+NotGlebO@users.noreply.github.com>
2022-01-31 14:35:53 +0200

    Update turtle1.py

--- turtle1.py
+++ turtle1.py
@@ -1,11 +1,20 @@
 import turtle
-star = turtle.Turtle()
- 
-star.right(75)
-star.forward(100)
+
+t = turtle.Pen()
+t.color('red')
+t.circle(50)
+t.color('white')
+t.forward(100)
+t.color('blue')
+t.circle(50, steps=4)
+t.color('white')
+t.forward(25)
+t.color('green')
+t.right(75)
+t.forward(100)
  
 for i in range(4):
-    star.right(144)
-    star.forward(100)
+    t.right(144)
+    t.forward(100)
      
 turtle.done()
