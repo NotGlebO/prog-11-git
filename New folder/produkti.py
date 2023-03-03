8abaf39858e93e97bc23343e34a3b0c7d15d5f97
NotGlebO <77108978+NotGlebO@users.noreply.github.com>
2023-03-03 15:38:59 +0200

    Add files via upload

--- New folder/produkti.py
+++ New folder/produkti.py
@@ -61,13 +61,16 @@ except sq.Error as error:
 #Administratora panelis 
 def administratora_panelis():
 
+    global users_tree, frame2, frame3, search_user_entry, frame_check_prefix, users_dict, combobox_users, instrument_tree
+
     admin_window = Tk()
     admin_window.title('Administratora panelis')
-    admin_window.geometry('500x500')
+    admin_window.geometry('1200x700')
 
     notebook = ttk.Notebook(admin_window)
     notebook.grid(pady=10)
-    global frame2
+    
+    global frame1
     frame1 = ttk.Frame(notebook, width=500, height=480)
     frame2 = ttk.Frame(notebook, width=500, height=480)
     frame3 = ttk.Frame(notebook, width=500, height=480)
@@ -79,8 +82,68 @@ def administratora_panelis():
     notebook.add(frame3, text='Administratoru Tabula')
 
     
+
+    #Frame 1
+    
+    
+    
+    instrumen_columns = ("Nosaukums", 
+    "Kategorija", 
+    "Teh. raksturojums", 
+    'Pieejams',
+    'Cena',
+    'Instrumenta id'
+    )
+    
+    global instrument_tree
+    instrument_tree = ttk.Treeview(frame1, column=instrumen_columns, show='headings', height=10)
+    instrument_tree.grid(column=0, row=0)
+    
+
+
+    instrument_tree.heading('Nosaukums', text='Nosaukums')
+    instrument_tree.heading('Teh. raksturojums', text='Teh. raksturojums')
+    instrument_tree.heading('Kategorija', text='Kategorija')
+    instrument_tree.heading('Pieejams', text='Pieejams')
+    instrument_tree.heading('Cena', text='Cena')
+    instrument_tree.heading('Instrumenta id', text='Instrumenta id')
+    
+    for i in cursor.execute('SELECT * FROM produkts'):
+        instrument_tree.insert('', 'end', text="1", values=i)
+    
+    
+    global entry_nosaukums, entry_rakturojums, entry_kategorija, entry_pieejams, entry_cena, entry_instrumenta_id
+    label_nosaukums = Label(frame1, text='Nosaukums')
+    entry_nosaukums = Entry(frame1, width=15)
+    label_raksturojums = Label(frame1, text='Raksturojums')
+    entry_rakturojums = Text(frame1, width=50, height=10)
+    label_kategorija = Label(frame1, text='Kategorija')
+    entry_kategorija = Entry(frame1, width=15)
+    label_pieejams = Label(frame1, text='Kategorija')
+    entry_pieejams = Entry(frame1, width=15)
+    label_cena = Label(frame1, text='Cena')
+    entry_cena = Entry(frame1)
+    label_istrumenta_id = Label(frame1, text='Instrumenta_id')
+    entry_instrumenta_id = Entry(frame1)
+    instrument_add = Button(frame1, text='Add instrumentu', command=add_instrument)
+    instrument_remove = Button(frame1, text='Remove instrumentu', command=remove_instrument)
+
+    label_nosaukums.grid()
+    entry_nosaukums.grid()
+    label_raksturojums.grid()
+    entry_rakturojums.grid()
+    label_kategorija.grid()
+    entry_kategorija.grid()
+    label_pieejams.grid()
+    entry_pieejams.grid()
+    label_cena.grid()
+    entry_cena.grid()
+    label_istrumenta_id.grid()
+    entry_instrumenta_id.grid()
+    instrument_add.grid()
+    instrument_remove.grid()
     #Frame 2
-    global frame_check_prefix
+    
     frame_add_user = LabelFrame(frame2, text='Add user')
     frame_add_user.grid(column=0, row=0, padx=10)
 
@@ -91,7 +154,7 @@ def administratora_panelis():
     
     
     
-    global users_dict
+    
     users_dict = {}
     for i in cursor.execute('SELECT login, prefix FROM personals'):
         list(i)
@@ -101,7 +164,7 @@ def administratora_panelis():
     
    
 
-    global combobox_users
+     
     combobox_users = Combobox(frame_check_prefix)
     combobox_users['values'] = [*users_dict.keys()]
     combobox_users['state'] = ['readonly']
@@ -111,6 +174,8 @@ def administratora_panelis():
     Label(frame_check_prefix, text=' ', font=(35)).grid(column=3,row=4)
     Button(frame_check_prefix, text='Dot prefiksu', command=add_prefix).grid(column=2, row=5)
     Button(frame_check_prefix, text='Atņemt prefiksu', command=remove_prefix).grid(column=3, row=5)
+
+
 
 
     global user_vards, user_uzvards, user_login, user_parole, user_prefix
@@ -140,12 +205,14 @@ def administratora_panelis():
     user_prefix.grid(column=12, row=28)
     save_button.grid(column=12, row=32)
     
+    #Frame 3
+
     columns = ("Vārds", 
     "Uzvārds", 
     "login", 
     'prefix'
     )
-    global users_tree
+    
     users_tree = ttk.Treeview(frame3, column=columns, show='headings', height=5)
     users_tree.heading('Vārds', text='Vārds')
     users_tree.heading('Uzvārds', text='Uzvārds')
@@ -156,23 +223,65 @@ def administratora_panelis():
     for i in cursor.execute('SELECT vards, uzvards, login, prefix FROM personals'):
         users_tree.insert('', 'end', text="1", values=i)
     
-    users_tree.grid()
-
-    Button(frame3, text='Meklēt', command=search_button).grid( padx=100)
-    Button(frame3, text='Delete', command=delete).grid(padx=50)
+    users_tree.grid(column=0, row=0)
 
     
+    frame_search_user = LabelFrame(frame3, text='Add user')
+    frame_search_user.grid(pady=14)
+
+    Label(frame_search_user, text='Uzvārds').grid(padx=2) 
+    search_user_entry = Entry(frame_search_user)
+    search_user_entry.grid()  
+    Button(frame_search_user, text='Meklēt', command=search_button_users).grid(pady=15) 
+    
+    Button(frame3, text='Delete', command=delete_user).grid(padx=50)
+
     
     
-    
+def add_instrument():
+
+    sqlite_connection.execute(f'INSERT INTO produkts VALUES ("{entry_nosaukums.get()}", "{entry_kategorija.get()}", "{entry_rakturojums.get("1.0",)}", "{entry_pieejams.get()}", "{entry_cena.get()}", "{entry_instrumenta_id.get()}")')
+    sqlite_connection.commit()
     
 
-def search_button():
-    search_menu = Tk()
-    search_menu.geometry('200x100')
-    search_menu.title('Meklēt')
-    Entry(search_menu)    
-def delete():
+  
+    instrument_tree.insert('', 'end', text="1", values=(entry_nosaukums.get(), entry_kategorija.get(),entry_rakturojums.get("1.0",), entry_pieejams.get(), entry_cena.get(),entry_instrumenta_id.get() ) )
+
+
+def remove_instrument():
+    focus_item = instrument_tree.focus()
+    selected_item = instrument_tree.selection()[0]
+    item_details = instrument_tree.item(focus_item)
+    
+    instrument_tree.delete(selected_item)
+    cursor.execute(f'DELETE FROM produkts WHERE instruments_id = "{item_details.get("values")[5]}"') 
+    sqlite_connection.commit()
+
+def search_button_users():
+
+    if search_user_entry.get() == '':
+        searched_uzvards = cursor.execute('SELECT vards, uzvards, login, prefix FROM personals')
+    else:
+        searched_uzvards = cursor.execute(f'SELECT vards, uzvards, login, prefix FROM personals WHERE uzvards LIKE "{search_user_entry.get()}"')
+    columns = ("Vārds", 
+    "Uzvārds", 
+    "login", 
+    'prefix'
+    )
+    
+    users_tree = ttk.Treeview(frame3, column=columns, show='headings', height=5)
+    users_tree.heading('Vārds', text='Vārds')
+    users_tree.heading('Uzvārds', text='Uzvārds')
+    users_tree.heading('login', text='Login')
+    users_tree.heading('prefix', text='Prefix')
+
+    
+    for i in searched_uzvards:
+        users_tree.insert('', 'end', text="1", values=i)
+    
+    users_tree.grid(column=0, row=0)
+     
+def delete_user():
     focus_item = users_tree.focus()
     selected_item = users_tree.selection()[0]
     item_details = users_tree.item(focus_item)
@@ -235,11 +344,56 @@ def klientu_tabula():
     
 
     window_klient.destroy
-
+    global main_tk
     main_tk = Tk()
 
     main_tk.title('Klientu bāze')
-    main_tk.geometry('1600x300')
+    main_tk.geometry('1800x300')
+    columns = ('klientu ID',
+    "Vārds", 
+    "Uzvārds", 
+    "Personas kods", 
+    'Telefona Numurs',
+    'Valstu numura kods',
+    'Sakuma datums',
+    'Beidzuma datums',
+    'Instruments_id'
+    )
+    global tree
+    tree = ttk.Treeview(main_tk, column=columns, show='headings', height=5)
+    tree.heading('klientu ID', text='klientu ID')
+    tree.heading('Vārds', text='Vārds')
+    tree.heading('Uzvārds', text='Uzvārds')
+    tree.heading('Personas kods', text='Personas kods')
+    tree.heading('Telefona Numurs', text='Telefona Numurs')
+    tree.heading('Valstu numura kods', text='Valstu numura kods')
+    tree.heading('Sakuma datums', text='Sakuma datums')
+    tree.heading('Beidzuma datums', text='Beidzuma datums')
+    tree.heading('Instruments_id', text='Instruments_id')
+
+    for i in cursor.execute('SELECT * FROM klients'):
+        tree.insert('', 'end', text="1", values=i)
+    
+    tree.grid()
+    frame_search_klient = LabelFrame(main_tk, text='Add user')
+    frame_search_klient.grid(pady=14)
+
+    global search_klients_entry
+    Label(frame_search_klient, text='Uzvārds').grid(padx=2) 
+    search_klients_entry = Entry(frame_search_klient)
+    search_klients_entry.grid() 
+
+    Button(frame_search_klient, text='Meklēt Klientu', command=search_button_kliens).grid()
+    Button(main_tk, text='Delete', command=delete_klients).grid()
+
+
+def search_button_kliens():
+
+
+    if search_klients_entry.get() == '':
+        searched_uzvards = cursor.execute('SELECT * FROM klients')
+    else:
+        searched_uzvards = cursor.execute(f'SELECT * FROM klients WHERE uzvards LIKE "{search_klients_entry.get()}"')
     columns = ('klientu ID',
     "Vārds", 
     "Uzvārds", 
@@ -259,25 +413,35 @@ def klientu_tabula():
     tree.heading('Sakuma datums', text='Sakuma datums')
     tree.heading('Beidzuma datums', text='Beidzuma datums')
     
-
-    for i in cursor.execute('SELECT * FROM klients'):
+    for i in searched_uzvards:
         tree.insert('', 'end', text="1", values=i)
     
-    
-
-    tree.pack()
-
-
+    tree.grid(column=0, row=0)
 #Klientu saglabašana
 def saglabasana_datne():
-    pedeja_id = len(cursor.execute('SELECT * FROM klients').fetchall())
+    pedeja_id = cursor.execute('SELECT klientu_id FROM klients').fetchall()
+    for i in max(pedeja_id):
+        try:
+            pedeja_id = i
+        except:
+            pass
     try:
-        sqlite_connection.execute(f'INSERT INTO klients VALUES ({pedeja_id+1},"{entry_vards.get()}", "{entry_uzvards.get()}", "{entry_personas_kods.get()}", "{entry_telefona_numurs.get()}","{entry_telefona_kods.get()}", "{entry_sakuma_datums.get()}", "{entry_beiguma_datums.get()}")')
+        sqlite_connection.execute(f'INSERT INTO klients VALUES ({pedeja_id+1},"{entry_vards.get()}", "{entry_uzvards.get()}", "{entry_personas_kods.get()}", "{entry_telefona_numurs.get()}","{entry_telefona_kods.get()}", "{entry_sakuma_datums.get()}", "{entry_beiguma_datums.get()}", "{entry_instruments_id}")')
         sqlite_connection.commit()
         Label(window_klient, text='Datne saglabojas', fg='green').grid(column=4, row=0)
         Label(window_klient, text=f'Klientu {entry_vards.get()} {entry_uzvards.get()} kods: {pedeja_id+1}', fg='green').grid(column=4, row=1)
     except:
         Label(window_klient, text=f'Error: {sq.Error}', fg='red').grid(column=4, row=0)
+
+def delete_klients():
+
+    focus_item = tree.focus()
+    selected_item = tree.selection()[0]
+    item_details = tree.item(focus_item)
+    
+    tree.delete(selected_item)
+    cursor.execute(f'DELETE FROM klients WHERE klientu_id = "{item_details.get("values")[0]}"') 
+    sqlite_connection.commit()
 
 #Klientu registrešana
 def klientu_registresana():
@@ -293,6 +457,7 @@ def klientu_registresana():
     global entry_sakuma_datums
     global entry_beiguma_datums
     global entry_telefona_kods
+    global entry_instruments_id
 
     Label(window_klient, text='Vārds').grid(column=2, row=0 )
     entry_vards = Entry(window_klient, width=15)
@@ -331,9 +496,15 @@ def klientu_registresana():
     entry_beiguma_datums.grid(column=2, row=17)
 
 
+    Label(window_klient, text='Instrumentu ID').grid(column=3, row=0 )
+    entry_instruments_id = Entry(window_klient, width=16)
+    entry_instruments_id.grid(column=3, row=1)
+
     Button(window_klient, text='Saglabāt', command=saglabasana_datne).grid(column=1, row=18)
     Button(window_klient, text='Paradīt klientus', command=klientu_tabula).grid(column=2, row=18)
     Button(window_klient, text='Aizslēgt', command=window_klient.destroy).grid(column=3, row=18)
+
+
 
 
     
